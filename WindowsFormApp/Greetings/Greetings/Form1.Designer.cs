7a627ee726035098365db11839b384411b5b53db
unknown <schmidt1519@gmail.com>
2021-11-19 22:35:41 -0600

    Added Windows Form app

---
+++ WindowsFormApp/Greetings/Greetings/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Greetings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickHere = new System.Windows.Forms.Button();
+            this.labelGreetings = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClickHere
+            // 
+            this.btnClickHere.Location = new System.Drawing.Point(387, 192);
+            this.btnClickHere.Name = "btnClickHere";
+            this.btnClickHere.Size = new System.Drawing.Size(75, 23);
+            this.btnClickHere.TabIndex = 0;
+            this.btnClickHere.Text = "Click Here";
+            this.btnClickHere.UseVisualStyleBackColor = true;
+            this.btnClickHere.Click += new System.EventHandler(this.btnClickHere_Click);
+            // 
+            // labelGreetings
+            // 
+            this.labelGreetings.AutoSize = true;
+            this.labelGreetings.Location = new System.Drawing.Point(403, 269);
+            this.labelGreetings.Name = "labelGreetings";
+            this.labelGreetings.Size = new System.Drawing.Size(35, 13);
+            this.labelGreetings.TabIndex = 1;
+            this.labelGreetings.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelGreetings);
+            this.Controls.Add(this.btnClickHere);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickHere;
+        private System.Windows.Forms.Label labelGreetings;
+    }
+}
+
