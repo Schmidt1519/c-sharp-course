b6fcd9ae0381ff0d2685c19ee77138bfa283c89e
unknown <schmidt1519@gmail.com>
2021-11-21 22:20:00 -0600

    Added button with click event message box displaying text based on a conditional statement.

---
+++ Conditional/Conditional/Conditional/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conditional
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int finalGrades = 59;
+            if(finalGrades >= 75){
+                MessageBox.Show("First class");
+            } else{
+                MessageBox.Show("You got a Second class");
+            }
+
+        }
+    }
+}
