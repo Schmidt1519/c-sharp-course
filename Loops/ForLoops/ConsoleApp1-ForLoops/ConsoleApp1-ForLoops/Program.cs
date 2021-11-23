6278e07ab9e32f1eebbf1ab57192bec4479eb4b9
unknown <schmidt1519@gmail.com>
2021-11-22 23:40:07 -0600

    Added a for loop with a counter that increments and writes to console using string interpolation.

---
+++ Loops/ForLoops/ConsoleApp1-ForLoops/ConsoleApp1-ForLoops/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int x = 17; x < 27; x = x + 1)
+            {
+                Console.WriteLine("The value of x: {0}", x);
+            }
+            Console.ReadLine();
+        }
+    }
+}
