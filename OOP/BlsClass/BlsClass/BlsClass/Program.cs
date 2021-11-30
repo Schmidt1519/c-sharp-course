03218d6ad8ee49eb879bab2f58c7b83ae48b79d6
unknown <schmidt1519@gmail.com>
2021-11-29 23:01:08 -0600

    Created a class with static and non static members. Instantiated an object and accessed both the static and non static variables and methods.

---
+++ OOP/BlsClass/BlsClass/BlsClass/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BlsClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            MyClass classA = new MyClass();
+
+            Console.WriteLine(classA.message);
+            classA.Name = "Simon";
+            classA.DisplayName();
+
+            Console.WriteLine(MyClass.greetings);
+            MyClass.Age = 21;
+            MyClass.DisplayAge();
+
+            Console.Read();
+        }
+    }
+
+    class MyClass
+    {
+        // Non static members
+        public string message = "Hello World";
+        public string Name { get; set; }
+        public void DisplayName()
+        {
+            Console.WriteLine("Name = {0}", Name);
+        }
+
+        // static members
+        public static string greetings = "Good Morning";
+        public static int Age { get; set; }
+        public static void DisplayAge()
+        {
+            Console.WriteLine("Age = {0}", Age);
+        }
+    }
+}
