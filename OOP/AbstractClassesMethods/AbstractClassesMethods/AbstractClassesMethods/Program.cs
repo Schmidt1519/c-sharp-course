8cc3b5e05955abc3253746b6a5330dc4762fcd4a
unknown <schmidt1519@gmail.com>
2021-12-01 10:36:36 -0600

    Implemented abstraction - created an abstract class with two methods, one being abstract. Created a derived class with an override method. Instantiated new objects and called method from parent class, as well as the override method from the derived class.

---
+++ OOP/AbstractClassesMethods/AbstractClassesMethods/AbstractClassesMethods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AbstractClassesMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // MyAbstractClass abClass = new MyAbstractClass();
+
+            ClassA a = new ClassA();
+            a.PrintMessage();
+            a.PrintMessageAbstract();
+            Console.Read();
+
+        }
+    }
+
+    abstract class MyAbstractClass
+    {
+        private string message = "Good morning!";
+        public void PrintMessage()
+        {
+            Console.WriteLine(message);
+        }
+
+        public abstract void PrintMessageAbstract();
+    }
+
+    class ClassA : MyAbstractClass
+    {
+        public override void PrintMessageAbstract()
+        {
+            Console.WriteLine("Simplicity is the ultimate sophistication");
+        }
+    }
+}
