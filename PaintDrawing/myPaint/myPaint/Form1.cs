ad218402b06c6cdda3e08467f4a0cd45ef4e158e
Schmidt1519 <schmidt1519@gmail.com>
2021-12-13 21:26:00 -0600

    Creating a paint drawing application. Added menuStrip and toolStrip to a Form and updated Text, ForeColor, and DisplayStyle. Added PictureBox and set Dock to Fill. Created a Bitmap with pixel data. Created a Pen variable and a boolean for isDrawing. Added event handler method to the pictureBox.

---
+++ PaintDrawing/myPaint/myPaint/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myPaint
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp = new Bitmap(1024, 768);
+        Pen p = new Pen(Color.Black, 5);
+        bool isDrawing = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+                isDrawing = false;
+            else
+                isDrawing = true;
+        }
+    }
+}
